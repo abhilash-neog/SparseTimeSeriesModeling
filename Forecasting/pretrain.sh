eb9efcb568025c4cc02ede7057f701176fe8c1bb
abhilash97 <abhineog97@gmail.com>
2024-03-15 23:51:41 -0400

    updated gitignore

--- Forecasting/pretrain.sh
+++
@@ -1,9 +0,0 @@
-python -u executor.py \
-    --task_name pretrain \
-    --ckpt_name ett_v0.pth \
-    --device 2 \
-    --run_name ettm2_pretrain_initial \
-    --source_filename ETTm2 \
-    --dataset ETT \
-    --max_epochs 2 \
-    --encoder_embed_dim 96
