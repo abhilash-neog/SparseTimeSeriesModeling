d0434c8446418b89d51e30643654948b8ff8a2f5
Abhilash Neog <abhilash22@tc-dgx007.cm.cluster>
2024-09-21 19:59:44 -0400

    updated baseline scripts for ecl

--- Forecasting/Baselines/SimMTM/scripts/ECL_script/ECL_masked_SAITS.sh
+++ Forecasting/Baselines/SimMTM/scripts/ECL_script/ECL_masked_SAITS.sh
@@ -12,21 +12,21 @@ source activate env
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.conda/envs/env/lib
 
-BASE_PATH="/projects/ml4science/time_series/ts_synthetic_datasets/synthetic_datasets/ETTh2/"
+BASE_PATH="/projects/ml4science/time_series/ts_synthetic_datasets/synthetic_datasets/electricity/"
 ROOT_PATHS=$1
 DEVICES=$2
 TRIAL=$3
 MASKINGTYPE=$4
 PRED_LEN_LIST=$5
 
-DATA_PATH="v${TRIAL}_${MASKINGTYPE}_etth2_imputed_SAITS.csv"
+DATA_PATH="v${TRIAL}_${MASKINGTYPE}_electricity_imputed_SAITS.csv"
 
-GT_ROOT_PATH="/projects/ml4science/time_series/ts_forecasting_datasets/ETT/"
+GT_ROOT_PATH="/projects/ml4science/time_series/ts_forecasting_datasets/electricity/"
 
 PRETRAIN_CHECKPOINTS_DIR="/projects/ml4science/time_series/SimMTM/SAITS/pretrain_checkpoints/"
 FINETUNE_CHECKPOINTS_DIR="/projects/ml4science/time_series/SimMTM/SAITS/finetune_checkpoints/"
 
-OUTPUT_PATH="/projects/ml4science/time_series/SimMTM/outputs/SAITS/${MASKINGTYPE}/ETTh2_v${TRIAL}/"
+OUTPUT_PATH="/projects/ml4science/time_series/SimMTM/outputs/SAITS/${MASKINGTYPE}/ECL_v${TRIAL}/"
 
 IFS=',' read -r -a PRED_LEN_ARRAY <<< "$PRED_LEN_LIST"
 
@@ -35,59 +35,57 @@ for id in $ROOT_PATHS; do
     root_path="${BASE_PATH}${id}"
     python -u run.py \
         --task_name pretrain \
-        --root_path $root_path\
+        --root_path $root_path \
         --data_path $DATA_PATH \
-        --model_id ETTh2 \
+        --model_id ECL \
         --model SimMTM \
-        --data ETTh2 \
+        --data ECL \
         --features M \
         --seq_len 336 \
+        --label_len 48 \
         --e_layers 2 \
-        --enc_in 7 \
-        --dec_in 7 \
-        --c_out 7 \
-        --n_heads 8 \
-        --d_model 8 \
-        --d_ff 32 \
-        --positive_nums 3 \
+        --positive_nums 2 \
         --mask_rate 0.5 \
-        --learning_rate 0.001 \
-        --batch_size 16 \
-        --train_epochs 50 \
+        --enc_in 321 \
+        --dec_in 321 \
+        --c_out 321 \
+        --d_model 32 \
+        --d_ff 64 \
+        --n_heads 16 \
+        --batch_size 32 \
+        --train_epochs 1 \
+        --temperature 0.02 \
         --trial $TRIAL \
         --pretrain_checkpoints $PRETRAIN_CHECKPOINTS_DIR \
         --gpu $DEVICES
     
-    for pred_len in ${PRED_LEN_ARRAY[@]}; do
+    for pred_len in 96 192 336 720; do
         python -u run.py \
             --task_name finetune \
             --gt_root_path $GT_ROOT_PATH \
             --root_path $root_path \
             --data_path $DATA_PATH \
-            --gt_data_path ETTh2.csv \
-            --is_training 1 \
-            --model_id ETTh2 \
+            --gt_data_path electricity.csv \
+            --model_id ECL \
             --model SimMTM \
-            --data ETTh2 \
+            --data ECL \
             --features M \
             --seq_len 336 \
             --label_len 48 \
             --pred_len $pred_len \
             --e_layers 2 \
-            --enc_in 7 \
-            --dec_in 7 \
-            --c_out 7 \
-            --n_heads 8 \
-            --d_model 8 \
-            --d_ff 32 \
-            --dropout 0.4 \
-            --head_dropout 0.2 \
-            --batch_size 16 \
+            --enc_in 321 \
+            --dec_in 321 \
+            --c_out 321 \
+            --d_model 32 \
+            --d_ff 64 \
+            --n_heads 16 \
+            --batch_size 32 \
             --gpu $DEVICES \
             --trial $TRIAL \
+            --train_epochs 1\
             --pretrain_checkpoints $PRETRAIN_CHECKPOINTS_DIR \
             --checkpoints $FINETUNE_CHECKPOINTS_DIR \
-            --output_path $OUTPUT_PATH \
-            --train_epochs 10
+            --output_path $OUTPUT_PATH
     done
 done
