d772da8169090408ed09beb1f2e8c6347a809517
Abhilash Neog <abhilash22@tc-dgx007.cm.cluster>
2024-09-25 04:01:38 -0400

    updated weather scripts

---
+++ Forecasting/Baselines/Autoformer/scripts/ETT_script/ARC_scripts/Autoformer_ETTh2_masked_Spline.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH -J etth2testing
+#SBATCH --account=ml4science
+#SBATCH --partition=dgx_normal_q #dgx_normal_q #a100_normal_q
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8
+#SBATCH --time=14:00:00 # 24 hours
+#SBATCH --gres=gpu:1
+
+module reset
+module load Anaconda3/2020.11
+source activate env
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.conda/envs/env/lib
+
+ROOT_PATHS=$1
+DEVICES=$2
+TRIAL=$3
+MASKINGTYPE=$4
+
+CHECKPOINT="/projects/ml4science/time_series/Autoformer/Spline/checkpoints/"
+GT_ROOT_PATH="/projects/ml4science/time_series/ts_forecasting_datasets/ETT/"
+
+root_path_name="/projects/ml4science/time_series/ts_synthetic_datasets/synthetic_datasets/ETTh2/"
+data_path_name="v${TRIAL}_${MASKINGTYPE}_etth2_imputed.csv"
+
+OUTPUT_PATH="/projects/ml4science/time_series/Autoformer/outputs/Spline/${MASKINGTYPE}/ETTh2_v${TRIAL}/"
+
+seq_len=336
+
+model_name=Autoformer
+
+for id in $ROOT_PATHS; do
+    root_path="${root_path_name}${id}"
+    for pred_len in 96 192 336 720; do
+        python -u run.py \
+          --is_training 1 \
+          --root_path $root_path \
+          --data_path $data_path_name \
+          --gt_root_path $GT_ROOT_PATH \
+          --gt_data_path ETTh2.csv \
+          --model_id "ETTh2_336_${pred_len}" \
+          --model Autoformer \
+          --data ETTh2 \
+          --features M \
+          --seq_len $seq_len \
+          --label_len 168 \
+          --pred_len $pred_len \
+          --e_layers 2 \
+          --d_layers 1 \
+          --factor 3 \
+          --enc_in 7 \
+          --dec_in 7 \
+          --c_out 7 \
+          --des 'Exp' \
+          --itr 1 \
+          --gpu $DEVICES \
+          --checkpoints $CHECKPOINT \
+          --output_path $OUTPUT_PATH
+    done
+done
--- Forecasting/Baselines/Autoformer/scripts/Weather_script/ARC_scripts/Autoformer_weather_masked_Spline.sh
+++ Forecasting/Baselines/Autoformer/scripts/Weather_script/ARC_scripts/Autoformer_weather_masked_Spline.sh
@@ -2,8 +2,8 @@
 #SBATCH -J etth2testing
 #SBATCH --account=ml4science
 #SBATCH --partition=dgx_normal_q #dgx_normal_q #a100_normal_q
-#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=16
-#SBATCH --time=3:00:00 # 24 hours
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8
+#SBATCH --time=14:00:00 # 24 hours
 #SBATCH --gres=gpu:1
 
 module reset
@@ -17,16 +17,17 @@ DEVICES=$2
 TRIAL=$3
 MASKINGTYPE=$4
 
-CHECKPOINT="/projects/ml4science/time_series/iTransformer/SAITS/checkpoints/"
+CHECKPOINT="/projects/ml4science/time_series/Autoformer/Spline/checkpoints/"
 GT_ROOT_PATH="/projects/ml4science/time_series/ts_forecasting_datasets/weather/"
 
 root_path_name="/projects/ml4science/time_series/ts_synthetic_datasets/synthetic_datasets/weather/"
-data_path_name="v${TRIAL}_${MASKINGTYPE}_weather_imputed_SAITS.csv"
+data_path_name="v${TRIAL}_${MASKINGTYPE}_weather_imputed.csv"
 
-OUTPUT_PATH="/projects/ml4science/time_series/iTransformer/outputs/SAITS/${MASKINGTYPE}/weather_v${TRIAL}/"
+OUTPUT_PATH="/projects/ml4science/time_series/Autoformer/outputs/Spline/${MASKINGTYPE}/weather_v${TRIAL}/"
+
 seq_len=336
 
-model_name=iTransformer
+model_name=Autoformer
 
 for id in $ROOT_PATHS; do
     root_path="${root_path_name}${id}"
@@ -37,22 +38,22 @@ for id in $ROOT_PATHS; do
           --data_path $data_path_name \
           --gt_root_path $GT_ROOT_PATH \
           --gt_data_path weather.csv \
-          --model_id "weather_${seq_len}_${pred_len}" \
-          --model $model_name \
+          --model_id "weather_336_${pred_len}" \
+          --model Autoformer \
           --data weather \
           --features M \
           --seq_len $seq_len \
+          --label_len 168 \
           --pred_len $pred_len \
-          --e_layers 3 \
+          --e_layers 2 \
+          --d_layers 1 \
+          --factor 3 \
           --enc_in 21 \
           --dec_in 21 \
           --c_out 21 \
           --des 'Exp' \
-          --d_model 512\
-          --d_ff 512 \
           --itr 1 \
           --gpu $DEVICES \
-          --trial $TRIAL \
           --checkpoints $CHECKPOINT \
           --output_path $OUTPUT_PATH
     done
