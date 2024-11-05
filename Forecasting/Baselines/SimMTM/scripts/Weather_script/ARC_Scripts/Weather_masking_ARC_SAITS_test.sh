68af5b28fef1005c657bdef3797e749b1a74990b
Abhilash Neog <abhilash22@tinkercliffs1.cm.cluster>
2024-11-05 05:35:07 -0500

    minor changes in SimMTM scripts

--- Forecasting/Baselines/SimMTM/scripts/Weather_script/ARC_Scripts/Weather_masking_ARC_SAITS_test.sh
+++ Forecasting/Baselines/SimMTM/scripts/Weather_script/ARC_Scripts/Weather_masking_ARC_SAITS_test.sh
@@ -35,30 +35,30 @@ IFS=',' read -r -a PRED_LEN_ARRAY <<< "$PRED_LEN_LIST"
 for id in $ROOT_PATHS; do
 
     root_path="${BASE_PATH}${id}"
-    python -u run.py \
-        --task_name pretrain \
-        --root_path $root_path \
-        --data_path $DATA_PATH \
-        --model_id Weather \
-        --model SimMTM \
-        --data Weather \
-        --features M \
-        --seq_len 336 \
-        --e_layers 2 \
-        --positive_nums 2 \
-        --mask_rate 0.5 \
-        --enc_in 21 \
-        --dec_in 21 \
-        --c_out 21 \
-        --n_heads 8 \
-        --d_model 64 \
-        --d_ff 64 \
-        --learning_rate 0.001 \
-        --batch_size 8 \
-        --train_epochs 50 \
-        --trial $TRIAL \
-        --pretrain_checkpoints $PRETRAIN_CHECKPOINTS_DIR \
-        --gpu $DEVICES
+    # python -u run.py \
+    #     --task_name pretrain \
+    #     --root_path $root_path \
+    #     --data_path $DATA_PATH \
+    #     --model_id Weather \
+    #     --model SimMTM \
+    #     --data Weather \
+    #     --features M \
+    #     --seq_len 336 \
+    #     --e_layers 2 \
+    #     --positive_nums 2 \
+    #     --mask_rate 0.5 \
+    #     --enc_in 21 \
+    #     --dec_in 21 \
+    #     --c_out 21 \
+    #     --n_heads 8 \
+    #     --d_model 64 \
+    #     --d_ff 64 \
+    #     --learning_rate 0.001 \
+    #     --batch_size 8 \
+    #     --train_epochs 50 \
+    #     --trial $TRIAL \
+    #     --pretrain_checkpoints $PRETRAIN_CHECKPOINTS_DIR \
+    #     --gpu $DEVICES
     
     for pred_len in ${PRED_LEN_ARRAY[@]}; do
         python -u run.py \
